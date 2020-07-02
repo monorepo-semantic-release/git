72a624fde11c7454dbfba4f95c067bd05b32ce8c
twinh <twinhuang@qq.com>
2020-07-02 14:27:25 +0800

    fix: set pluginConfig in verifyConditionsAll won't pass to next step

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const path = require('path');
 const {verifyConditions, prepare} = require('@semantic-release/git');
 const resolveConfig = require('@semantic-release/git/lib/resolve-config');
 const {isNil, uniq} = require('lodash');
+const debug = require('debug')('semantic-release:monnorepo-git');
 
-
-async function verifyConditionsAll(pluginConfig, context) {
+async function prepareAll(pluginConfig, context) {
   const {logger} = context;
-
-  await verifyConditions(pluginConfig, context);
 
   if (isNil(pluginConfig.assets)) {
     // Add "package.json" and "CHANGELOG.md" in package directories
@@ -16,12 +14,11 @@ async function verifyConditionsAll(pluginConfig, context) {
       path.join(pkg, 'package.json'),
       path.join(pkg, 'CHANGELOG.md'),
     ]).flat()));
+
+    debug('Use default assets: %O', pluginConfig.assets);
   }
-}
 
-async function prepareAll(pluginConfig, context) {
   // NOTE: generate placeholder variables for git plugin, may by remove in the future
-
   context.branch = context.pkgContexts[0].branch;
 
   const notes = [];
@@ -41,4 +38,4 @@ async function prepareAll(pluginConfig, context) {
   await prepare(pluginConfig, context);
 }
 
-module.exports = {verifyConditionsAll, prepareAll};
+module.exports = {verifyConditionsAll: verifyConditions, prepareAll};
