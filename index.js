dac800626f54857426b9a79cc8dbfca7bec29226
twinh <twinhuang@qq.com>
2020-08-14 11:48:06 +0800

    fix: Specify simplify commit title for monorepo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {isNil, uniq, forEach} = require('lodash');
 const debug = require('debug')('semantic-release:monnorepo-git');
 
 async function prepareAll(pluginConfig, context) {
-  const {options: {versionMode}, logger, pkgContexts} = context;
+  const {options, logger, pkgContexts} = context;
 
   if (isNil(pluginConfig.assets)) {
     // Add "package.json" and "CHANGELOG.md" in package directories
@@ -22,12 +22,14 @@ async function prepareAll(pluginConfig, context) {
   // NOTE: generate placeholder variables for git plugin, may by remove in the future
   context.branch = Object.values(pkgContexts)[0].branch;
 
+  const pkgs = [];
   const notes = [];
   const versions = [];
   forEach(pkgContexts, ({name, nextRelease}) => {
     if (!nextRelease) {
       return;
     }
+    pkgs.push(`- ${nextRelease.gitTag}`);
     notes.push(`# ${name}\n\n${nextRelease.notes}`);
     versions.push(nextRelease.gitTag);
   });
@@ -35,6 +37,11 @@ async function prepareAll(pluginConfig, context) {
   context.nextRelease = {
     version: uniq(versions).join(', '),
     notes: notes.join('\n\n'),
+  }
+
+  // Specify simplify commit title for monorepo
+  if (!pluginConfig.message && uniq(versions).length > 1) {
+    pluginConfig.message = `chore(release): publish [skip ci]\n\n${pkgs.join('\n')}\n\n${context.nextRelease.notes}`;
   }
 
   await prepare(pluginConfig, context);
